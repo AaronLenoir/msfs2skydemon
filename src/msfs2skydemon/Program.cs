0b8fd682b991c76a3017e585d2f73c8f0beec05f
AaronLenoir <lenoir.aaron@gmail.com>
2020-12-19 22:29:30 +0100

    Initial commit (hello world)

---
+++ src/msfs2skydemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace msfs2skydemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
